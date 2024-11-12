6b3e3443eb4ac05b146255a0d040ecf49c03a083
Lana Septiana <lana.septiana2@gmail.com>
2024-11-12 13:37:27 +0700

    install(jwt): install jwt package

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/migrate', function () {
+    Artisan::call('migrate');
+    
+    return 'Migrations are running...';
 });
